a8a7e4b2386dd87e61e246b8a6ecb1bb99c71f13
Heather Gray <heathergray153@gmail.com>
2022-06-26 12:16:06 -0600

    add test enum class with paths

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+class PropertyModel(str, Enum):
+    property1 = 'property1'
+    property2 = 'property2'
+    property3 = 'property3'
+
+
 @app.get("/")
 async def root():
     return {"message": "Hi Mom"}
+
+
+@app.get("/test/{test_property}")
+async def test(test_property: PropertyModel):
+    if test_property == PropertyModel.property1:
+        return {"property_name": "property1", "property_value": "value1"}
+    if test_property == PropertyModel.property2:
+        return {"property_name": "property2", "property_value": "value2"}
+    if test_property == PropertyModel.property3:
+        return {"property_name": "property3", "property_value": "value3"}
+
