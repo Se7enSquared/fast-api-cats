4f385719f58cfc0cb05f1fde3bc2192d6f6c960c
Heather Gray <heathergray153@gmail.com>
2022-06-27 14:45:08 -0600

    edit cat

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ class Cat(BaseModel):
     image: Optional[str] = None
 
 
+class UpdateCat(BaseModel):
+    name: Optional[str]
+    age: Optional[int]
+    color: Optional[str]
+    temperament: Optional[str]
+    weight_in_lbs: Optional[float]
+    description: Optional[str]
+    image: Optional[str] = None
+
+
 with open('cats.json') as f:
     cats = json.load(f)
 
@@ -35,7 +45,7 @@ def get_cat(cat_id: int = Path(None, description='The cat\'s id number')):
 def get_cat_by_name(*, name: Optional[str]):
     return next(
         (cats[cat_id] for cat_id in cats
-            if cats[cat_id]['name'].lower() == name.lower()),
+            if cats[cat_id].name.lower() == name.lower()),
         {'Data': 'Not found'}
     )
 
@@ -43,15 +53,16 @@ def get_cat_by_name(*, name: Optional[str]):
 @app.post("/add-cat/{cat_id}")
 def add_cat(cat_id: int, cat: Cat):
     if cat_id in cats:
-        return {'Error': 'Item id already exists'}
+        return {'Error': 'Cat id already exists'}
 
-    cats[cat_id] = {
-        'name': cat.name.title(),
-        'age': cat.age,
-        'color': cat.color.lower(),
-        'temperament': cat.temperament.lower(),
-        'weight_in_lbs': cat.weight_in_lbs,
-        'description': cat.description,
-        'image': cat.image
-    }
+    cats[cat_id] = cat
+    return cats[cat_id]
+
+
+@app.put("/edit-cat/{cat_id}")
+def edit_cat(cat_id: int, cat: UpdateCat):
+    if cat_id not in cats:
+        return {'Error': 'Cat id not found'}
+
+    cats[cat_id].update(cat)
     return cats[cat_id]
