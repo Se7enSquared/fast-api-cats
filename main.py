a49c94efb5f68f8ec4682ad78958efbd104b4003
Heather Gray <heathergray153@gmail.com>
2022-06-27 13:56:35 -0600

    start app for cat api

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-from enum import Enum
+import json
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-class PropertyModel(str, Enum):
-    property1 = 'property1'
-    property2 = 'property2'
-    property3 = 'property3'
+with open('cats.json') as f:
+    cats = json.load(f)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hi Mom"}
-
-
-@app.get("/test/{test_property}")
-async def test(test_property: PropertyModel):
-    if test_property == PropertyModel.property1:
-        return {"property_name": "property1", "property_value": "value1"}
-    if test_property == PropertyModel.property2:
-        return {"property_name": "property2", "property_value": "value2"}
-    if test_property == PropertyModel.property3:
-        return {"property_name": "property3", "property_value": "value3"}
+@app.get('/')
+def home():
+    return {'data': 'Welcome to CatAPI'}
 
