10f86fae58334caae579475fd549908719eccabf
Heather Gray <heathergray153@gmail.com>
2022-06-27 13:57:56 -0600

    add get cat by id

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ with open('cats.json') as f:
 def home():
     return {'data': 'Welcome to CatAPI'}
 
+
+@app.get('/get-cat/{cat_id}')
+def get_cat(cat_id: int = Path(None, description='The cat\'s id number')):
+    cat_id = str(cat_id)
+    return cats[cat_id]
+
