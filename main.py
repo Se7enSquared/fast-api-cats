8b9e00900bc76630c5a4b38feea92289a2bcb8a4
Bob Belderbos <bobbelderbos@gmail.com>
2022-06-29 16:48:10 +0200

    as per https://github.com/bbelderbos/affirmations-api/blob/main/affirmations/main.py

--- main.py
+++ main.py
@@ -95,10 +95,10 @@ def edit_cat(cat_id: int, cat: UpdateCat):
     return update_cat
 
 
-@app.delete("/del-cat/{cat_id}", status_code=204)
+@app.delete("/del-cat/{cat_id}")
 def del_cat(cat_id: int):
     if cat_id not in cats:
         raise HTTPException(status_code=404, detail="Cat id not found")
 
     del cats[cat_id]
-    return cats
+    return {"ok": True}
